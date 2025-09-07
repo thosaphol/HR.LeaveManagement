d15d6e267837f053f970d6808b3980e38915ff8b
thosaphol <thosaphol@outlook.co.th>
2025-09-07 22:45:52 +0700

    🎉Add Swagger for sample test api

--- src/API/HR.LeaveManagement.API/Program.cs
+++ src/API/HR.LeaveManagement.API/Program.cs
@@ -11,7 +11,10 @@ builder.Services.ConfigurePersistenceServices(builder.Configuration);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "HR.LeaveManagement.API", Version = "v1" });
+});
 
 builder.Services.AddCors(options =>
 {
@@ -27,7 +30,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "HR.LeaveManagement.API v1");
+    });
 }
 
 app.UseHttpsRedirection();
